e8c2acd64241dd275fcc1c48e5032cca4fcc5e47
Jon Moss <me@jonathanmoss.me>
2016-02-19 18:45:00 -0500

    Associate OAuth applications with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :oauth_applications, class_name: 'Doorkeeper::Application', as: :owner
+
   def admin!
     self.admin = true
     self.save!
