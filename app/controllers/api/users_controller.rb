b4e62ec7a32dcc5f9e6df4e9938fbd08ff32ab8b
Jon Moss <me@jonathanmoss.me>
2016-02-20 16:49:36 -0500

    Add PUT /api/users/me
    Also, modified POST /api/users to return a user access token in the
    response.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,7 +7,10 @@ module Api
       user = User.new(user_params)
 
       if user.save
-        render json: user, status: 201
+        oauth_app = Doorkeeper::Application.find_by_uid(params[:application_id])
+        token = Doorkeeper::AccessToken.create!(application_id: oauth_app.id, resource_owner_id: user.id)
+
+        render json: user.as_json.merge(access_token: token.token), status: 201
       else
         render json: user.errors, status: 422
       end
@@ -16,6 +19,17 @@ module Api
     # GET /api/users/me
     def me
       render json: current_api_user
+    end
+
+    # PUT /api/users/me
+    def update
+      current_api_user.update(user_params)
+
+      if current_api_user.save
+        render json: current_api_user, status: 200
+      else
+        render json: current_api_user.errors, status: 422
+      end
     end
 
     protected
--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -3,18 +3,30 @@ require 'rails_helper'
 RSpec.describe Api::UsersController, :type => :controller do
   describe "POST #create" do
     context "no access token" do
+      before do
+        @user_attributes = FactoryGirl.attributes_for(:user)
+        @user = FactoryGirl.create(:user)
+        @application = FactoryGirl.create(:oauth_application, owner_id: @user.id, owner_type: User)
+      end
+
       it "returns http 201" do
-        post :create, format: :json, user: FactoryGirl.attributes_for(:user)
+        post :create, format: :json, user: @user_attributes, application_id: @application.uid
         expect(response.status).to eq(201)
       end
 
-      it "returns correct http body" do
-        user_attributes = FactoryGirl.attributes_for(:user)
-        post :create, format: :json, user: user_attributes
+      it "creates a new profile" do
+        expect{
+          post :create, format: :json, user: @user_attributes, application_id: @application.uid
+        } .to change(User, :count).by(1)
+      end
 
+      it "returns correct http body" do
+        post :create, format: :json, user: @user_attributes, application_id: @application.uid
         resp = JSON.parse(response.body)
-        expect(resp.keys).to eq(["id", "email", "created_at", "updated_at", "admin"])
-        expect(resp["email"]).to eq(user_attributes[:email])
+
+        expect(resp.keys).to eq(["id", "email", "created_at", "updated_at", "admin", "access_token"])
+        expect(resp["email"]).to eq(@user_attributes[:email])
+        expect(resp["admin"]).to eq(@user_attributes[:admin])
       end
     end
   end
@@ -42,6 +54,37 @@ RSpec.describe Api::UsersController, :type => :controller do
       it "returns correct http body" do
         get :me, format: :json, access_token: @token.token
         expect(response.body).to eq(@user.to_json)
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "invalid access token" do
+      it "returns http 401" do
+        put :update
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context "valid access token" do
+      before do
+        @user = FactoryGirl.create(:user)
+        @application = FactoryGirl.create(:oauth_application, owner_id: @user.id, owner_type: User)
+        @token = FactoryGirl.create(:oauth_access_token, application: @application, resource_owner_id: @user.id)
+      end
+
+      it "returns http 200" do
+        put :update, format: :json, access_token: @token.token, user: FactoryGirl.attributes_for(:user)
+        expect(response.status).to eq(200)
+      end
+
+      it "updates user's attribute(s)" do
+        put :update, format: :json, access_token: @token.token, user: FactoryGirl.attributes_for(:user, email: "test@test.com")
+        body = JSON.load(response.body)
+
+        expect(body["id"]).to eq(@user.id)
+        expect(body["email"]).to eq("test@test.com")
+        expect(body["admin"]).to eq(false)
       end
     end
   end
